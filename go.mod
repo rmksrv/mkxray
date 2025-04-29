b5c10b60606a0ef786be730388e027924d3c9be0
Roman Kosarev <rmksrv@outlook.com>
2025-04-29 14:47:11 +0400

    Improved UI

--- go.mod
+++ go.mod
@@ -2,10 +2,12 @@ module github.com/rmksrv/mkxray
 
 go 1.24.2
 
-require github.com/fatih/color v1.18.0
+require github.com/muesli/termenv v0.16.0
 
 require (
-	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
+	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
-	golang.org/x/sys v0.25.0 // indirect
+	github.com/rivo/uniseg v0.4.7 // indirect
+	golang.org/x/sys v0.30.0 // indirect
 )
--- mkxray.go
+++ mkxray.go
@@ -8,147 +8,183 @@ import (
 	"os/exec"
 	"os/user"
 	"regexp"
+	"runtime"
 	"strings"
 	"time"
-
-	"github.com/fatih/color"
 )
 
 func main() {
-	ExitIfNotRoot()
-	UpdateSystem()
-	InstallXray()
-	PrintInfo("Generating items for xray\n")
-	ctx := NewXrayContext("www.samsung.com:443", "www.samsung.com")
-	cfg := MakeXrayConfig(ctx)
-	WriteConfigFile(cfg)
-	RestartXray()
-	PrintInfo("Xray service is ready for usage\n")
-	vlessLink := GenerateVlessLink(ctx, "mkxray", "xtls-rprx-vision", "raw", "reality", "edge")
-	PrintInfo(vlessLink + "\n")
-}
-
-func GenerateVlessLink(ctx *XrayContext, name, flow, typ, security, fp string) string {
-	PrintInfo("Generating vless link...\n")
-	link := fmt.Sprintf(
-		"vless://%s@%s:443?flow=%s&type=%s&security=%s&fp=%s&sni=%s&pbk=%s&sid=%s#%s",
-		ctx.clientID,
-		ctx.externalIP,
-		flow,
-		typ,
-		security,
-		fp,
-		ctx.serverName,
-		ctx.publicKey,
-		ctx.shortID,
-		name,
+	ctx := &XrayContext{}
+	app := InitApp(
+		"Setup mkxray, please wait...",
+		*CheckIfProperSystem(),
+		*CheckIfRoot(),
+		*DownloadXray(),
+		*InstallXray(),
+		*CheckXray(),
+		*GenerateXrayContext(ctx, "www.samsung.com:443", "www.samsung.com"),
+		*WriteXrayConfig(ctx),
+		*RestartXray(),
 	)
-	return link
-}
+	defer app.RestoreConsole()
 
-func ExitIfNotRoot() {
-	currentUser, err := user.Current()
-	HandleError(err, "Unable to get current user")
-	if currentUser.Username != "root" {
-		PrintErr("`mkxray` is supposed to be executed with elevated privileges. Please restart it using `sudo mkxray`\n")
-		os.Exit(1)
+	RenderUI(app, false)
+	for jobIdx := range app.Jobs {
+		job := &app.Jobs[jobIdx]
+		job.Status = IN_PROGRESS
+		RenderUI(app, true)
+		err := RunJob(job)
+		RenderUI(app, true)
+
+		if err != nil {
+			println(ErrorMsg(app, err.Error()))
+			os.Exit(1)
+		}
 	}
+	RenderUI(app, true)
+	RenderEndMessage(app, ctx)
 }
 
-func UpdateSystem() {
-	PrintInfo("Updating system...\n")
-	err := RunCmd("apt-get", "update", "-y")
-	HandleError(err, "Unable to update system")
-
-	PrintInfo("Upgrading system...\n")
-	err = RunCmd("apt-get", "upgrade", "-y")
-	HandleError(err, "Unable to upgrade system")
+func CheckIfProperSystem() *Job {
+	j := NewJob("Check system", func() error {
+		sys := runtime.GOOS
+		if sys != "linux" {
+			return fmt.Errorf("system is not linux")
+		}
+		return nil
+	})
+	return &j
 }
 
-func InstallXray() {
-	PrintInfo("Downloading Xray installer...\n")
-	resp, err := http.Get(XRAY_INSTALL_URL)
-	HandleError(err, "Unable to download xray installer")
-	bytes, err := io.ReadAll(resp.Body)
-	HandleError(err, "Unable to read xray installer")
-
-	installerPath := os.TempDir() + "/install-xray.sh"
-	err = os.WriteFile(installerPath, bytes, 0777)
-	HandleError(err, "Unable to write xray installer")
-
-	PrintInfo("Running installer...\n")
-	err = RunCmd(installerPath)
-	HandleError(err, "Unable to run xray installer")
-
-	PrintInfo("Verifying installation...\n")
-	out, err := exec.Command("xray", "--version", ">", "/dev/null", "2", ">", "&1", "&&", "echo", "0", "||", "echo", "1").Output()
-	HandleError(err, "Unable to verify xray installation")
-	if string(out[:]) == "1" {
-		fmt.Println()
-		PrintErr("Xray wasn't installed successfully; exiting...\n")
-		os.Exit(1)
-	}
+func CheckIfRoot() *Job {
+	j := NewJob("Check if root", func() error {
+		currentUser, err := user.Current()
+		if err != nil {
+			return fmt.Errorf("unable to get current user")
+		}
+		if currentUser.Username != "root" {
+			return fmt.Errorf("not root user")
+		}
+		return nil
+	})
+	return &j
 }
 
-func NewXrayContext(dest, serverName string) *XrayContext {
-	key, pubKey := NewXrayKeys()
-	ctx := XrayContext{
-		dest:       dest,
-		serverName: serverName,
-		privateKey: key,
-		publicKey:  pubKey,
-		clientID:   NewXrayUuid(),
-		shortID:    NewShortID(),
-		externalIP: GetExternalIP(),
-	}
-	return &ctx
+func DownloadXray() *Job {
+	j := NewJob("Download xray installer", func() error {
+		resp, err := http.Get(XRAY_INSTALL_URL)
+		if err != nil {
+			return fmt.Errorf("unable to download xray installer: %v", err)
+		}
+		defer resp.Body.Close()
+		if resp.StatusCode != http.StatusOK {
+			return fmt.Errorf("failed to download xray installer: %s", resp.Status)
+		}
+		bytes, err := io.ReadAll(resp.Body)
+		if err != nil {
+			return fmt.Errorf("unable to read xray installer: %v", err)
+		}
+		installerPath := os.TempDir() + "/install-xray.sh"
+		err = os.WriteFile(installerPath, bytes, 0777)
+		if err != nil {
+			return fmt.Errorf("unable to save xray installer: %v", err)
+		}
+		return nil
+	})
+	return &j
 }
 
-func MakeXrayConfig(ctx *XrayContext) string {
-	r := strings.NewReplacer(
-		"$dest$", ctx.dest,
-		"$clientID$", ctx.clientID,
-		"$serverName$", ctx.serverName,
-		"$privateKey$", ctx.privateKey,
-		"$shortID$", ctx.shortID,
-	)
-	return r.Replace(XRAY_CONFIG_TEMPLATE)
+func InstallXray() *Job {
+	j := NewJob("Install xray", func() error {
+		installerPath := os.TempDir() + "/install-xray.sh"
+		_, err := exec.Command(installerPath).Output()
+		if err != nil {
+			return fmt.Errorf("unable to run xray installer: %v", err)
+		}
+		return nil
+	})
+	return &j
 }
 
-func WriteConfigFile(cfg string) {
-	PrintInfo(fmt.Sprintf("Updating config file at `%s`\n", XRAY_CONFIG_PATH))
-	err := os.WriteFile(XRAY_CONFIG_PATH, []byte(cfg), 0444)
-	HandleError(err, "Unable to write xray config file")
+func CheckXray() *Job {
+	j := NewJob("Check xray installation", func() error {
+		out, err := exec.Command(
+			"xray", "--version", ">", "/dev/null", "2", ">", "&1",
+			"&&", "echo", "0",
+			"||", "echo", "1",
+		).Output()
+		if err != nil {
+			return fmt.Errorf("unable to verify xray installation: %v", err)
+		}
+		if string(out[:]) == "1" {
+			return fmt.Errorf("xray wasn't installed successfully")
+		}
+		return nil
+	})
+	return &j
 }
 
-func RestartXray() {
-	PrintInfo("Restarting xray...\n")
-	RunCmd("systemctl", "restart", "xray")
-	fmt.Println()
-	time.Sleep(2 * time.Second)
+func GenerateXrayContext(ctx *XrayContext, dest, serverName string) *Job {
+	j := NewJob("Generate xray context", func() error {
+		key, pubKey := NewXrayKeys()
+		ctx.Dest = dest
+		ctx.ServerName = serverName
+		ctx.PrivateKey = key
+		ctx.PublicKey = pubKey
+		ctx.ClientID = NewXrayUuid()
+		ctx.ShortID = NewShortID()
+		ctx.ExternalIP = GetExternalIP()
+		ctx.VlessLink = GenerateVlessLink(ctx, "mkxray", "xtls-rprx-vision", "raw", "reality", "edge")
+		return nil
+	})
+	return &j
+}
 
-	PrintInfo("Verifying xray is running fine...\n")
-	out, err := exec.Command(
-		"journalctl",
-		"-u", "xray",
-		"-n", "1",
-		"--no-pager",
-	).Output()
-	HandleError(err, "Unable to get xray logs")
-	re := regexp.MustCompile("core: Xray .* started")
-	if re.Find(out) == nil {
-		PrintErr("Something went wrong during xray restarting:\n")
-		out, _ = exec.Command(
+func WriteXrayConfig(ctx *XrayContext) *Job {
+	j := NewJob("Write Xray config", func() error {
+		cfg := strings.NewReplacer(
+			"$dest$", ctx.Dest,
+			"$clientID$", ctx.ClientID,
+			"$serverName$", ctx.ServerName,
+			"$privateKey$", ctx.PrivateKey,
+			"$shortID$", ctx.ShortID,
+		).Replace(XRAY_CONFIG_TEMPLATE)
+		err := os.WriteFile(XRAY_CONFIG_PATH, []byte(cfg), 0444)
+		if err != nil {
+			return fmt.Errorf("unable to write xray config file: %v", err)
+		}
+		return nil
+	})
+	return &j
+}
+
+func RestartXray() *Job {
+	j := NewJob("Restart xray", func() error {
+		_, err := exec.Command("systemctl", "restart", "xray").Output()
+		if err != nil {
+			return fmt.Errorf("unable to restart xray: %v", err)
+		}
+		time.Sleep(2 * time.Second)
+
+		out, err := exec.Command(
 			"journalctl",
 			"-u", "xray",
-			"-n", "6",
+			"-n", "1",
 			"--no-pager",
 		).Output()
-		fmt.Println(string(out))
-		PrintErr("Run `journalctl -u xray` to more details\n")
-		os.Exit(1)
-	}
+		if err != nil {
+			return fmt.Errorf("unable to get xray logs: %v", err)
+		}
+		re := regexp.MustCompile("core: Xray .* started")
+		if re.Find(out) == nil {
+			return fmt.Errorf("something went wrong during xray restarting: %s", out)
+		}
+		return nil
+	})
+	return &j
 }
+
+// internal
 
 const (
 	XRAY_INSTALL_URL     = "https://raw.githubusercontent.com/XTLS/Xray-install/046d9aa2432b3a6241d73c3684ef4e512974b594/install-release.sh"
@@ -227,59 +263,30 @@ const (
 }`
 )
 
-// internal
-
 type XrayContext struct {
-	dest       string
-	serverName string
-	privateKey string
-	publicKey  string
-	clientID   string
-	shortID    string
-	externalIP string
-}
-
-func HandleError(err error, msg string) {
-	if err != nil {
-		PrintErr(fmt.Sprintf("%s: %s\n", msg, err))
-		os.Exit(1)
-	}
-}
-
-func PrintLvl(lvl, msg string) {
-	fmt.Printf("[%s] %s", lvl, msg)
-}
-
-func PrintErr(msg string) {
-	red := color.New(color.FgRed).SprintFunc()
-	PrintLvl(red(" ERR"), msg)
-}
-
-func PrintInfo(msg string) {
-	hiCyan := color.New(color.FgHiCyan).SprintFunc()
-	PrintLvl(hiCyan("INFO"), msg)
-}
-
-func RunCmd(cmdName string, args ...string) error {
-	cyan := color.New(color.FgCyan).SprintFunc()
-	cmdString := cmdName + " " + strings.Join(args, " ")
-	PrintLvl(cyan(" CMD"), cmdString)
-
-	cmd := exec.Command(cmdName, args...)
-	cmd.Stdout = os.Stdout
-	err := cmd.Run()
-	return err
+	Dest       string
+	ServerName string
+	PrivateKey string
+	PublicKey  string
+	ClientID   string
+	ShortID    string
+	ExternalIP string
+	VlessLink  string
 }
 
 func NewXrayUuid() string {
 	out, err := exec.Command("xray", "uuid").Output()
-	HandleError(err, "Unable to generate UUID")
+	if err != nil {
+		panic(err)
+	}
 	return string(out[0 : len(out)-1])
 }
 
 func NewXrayKeys() (string, string) {
 	out, err := exec.Command("xray", "x25519").Output()
-	HandleError(err, "Unable to generate xray keys")
+	if err != nil {
+		panic(err)
+	}
 	re := regexp.MustCompile("Private key: (.+)\nPublic key: (.+)\n")
 	groups := re.FindStringSubmatch(string(out[:]))
 	private_key := groups[1]
@@ -289,13 +296,33 @@ func NewXrayKeys() (string, string) {
 
 func NewShortID() string {
 	out, err := exec.Command("openssl", "rand", "-hex", "8").Output()
-	HandleError(err, "Unable to generate short ID")
+	if err != nil {
+		panic(err)
+	}
 	return string(out[0 : len(out)-1])
 }
 
 func GetExternalIP() string {
-	PrintInfo("Getting external IP...\n")
 	out, err := exec.Command("dig", "+short", "myip.opendns.com", "@resolver1.opendns.com").Output()
-	HandleError(err, "Unable to get external IP")
+	if err != nil {
+		panic(err)
+	}
 	return string(out[0 : len(out)-1])
+}
+
+func GenerateVlessLink(ctx *XrayContext, name, flow, typ, security, fp string) string {
+	link := fmt.Sprintf(
+		"vless://%s@%s:443?flow=%s&type=%s&security=%s&fp=%s&sni=%s&pbk=%s&sid=%s#%s",
+		ctx.ClientID,
+		ctx.ExternalIP,
+		flow,
+		typ,
+		security,
+		fp,
+		ctx.ServerName,
+		ctx.PublicKey,
+		ctx.ShortID,
+		name,
+	)
+	return link
 }
